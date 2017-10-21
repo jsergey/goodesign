87dbb1645892ea115f4ab154ae639e53f104a290
Sergey Baranov <jsergey@gmail.com>
2017-10-22 01:16:29 +0300

    Initial script created

--- git-parser.py
+++ git-parser.py
@@ -1,11 +1,14 @@
 import re
 
+#git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames
 file = open("test.log", "r", encoding="UTF-8")
 lines = file.read().split("\n")
 
 commits = []
 changes = []
 commit = {}
+
+authors = set([])
 
 for nextLine in lines:
     if nextLine == '' or nextLine == '\n':
@@ -20,11 +23,28 @@ for nextLine in lines:
         commit['rev'] = m.group(1)
         commit['date'] = m.group(2)
         commit['author'] = m.group(3)
+        authors.add(commit['author'])
     else:
         m = re.compile('(\d*)\s*(\d*)\s*(.*)').match(nextLine)
         changes.append({'file' : m.group(3), 'added': m.group(1), 'deleted': m.group(2)})
 
-print(commits)
+print('Authors: '+ str(len(authors)) + '\n')
+
+number_of_authors = {}
+
+for commit in commits:
+    author = commit['author']
+    for entry in commit['changes']:
+        file = entry['file']
+        if(file in number_of_authors.keys()):
+            number_of_authors[file].add(author)
+        else:
+            number_of_authors[file] = set([author])
+
+for entry in number_of_authors.keys():
+    print (entry + '\t\t' + str(len(number_of_authors[entry])) + '\t' + str(number_of_authors[entry]))
+
+
 # [entry
 #  [revision]
 #  [author]
