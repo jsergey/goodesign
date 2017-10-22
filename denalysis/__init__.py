3478a066029dfd3d1d18672728fd4af5b0c31571
Sergey Baranov <jsergey@gmail.com>
2017-10-22 22:09:05 +0300

    init file created

---
+++ denalysis/__init__.py
@@ -0,0 +1,9 @@
+"""
+denalysis <https://github.com/jsergey/goodesign>`.
+"""
+
+__author__ = "Sergey Baranov"
+__copyright__ = "Copyright (C) 2017 Sergey Baranov"
+__license__ = "MIT License"
+__version__ = "0.1"
+__email__ = "jsergey@gmail.com"
