7f3550e97c22370b63ed032d25b8c5707457c9f0
Sergey Baranov <jsergey@gmail.com>
2017-10-22 21:52:40 +0300

    Initial script created

--- git-parser.py
+++ git-parser.py
@@ -1,5 +1,9 @@
 import re
 
+from calendar import monthrange
+from datetime import datetime, timedelta
+
+
 #git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames
 file = open("test.log", "r", encoding="UTF-8")
 lines = file.read().split("\n")
@@ -28,6 +32,7 @@ for nextLine in lines:
         m = re.compile('(\d*)\s*(\d*)\s*(.*)').match(nextLine)
         changes.append({'file' : m.group(3), 'added': m.group(1), 'deleted': m.group(2)})
 
+#authors
 print('Authors: '+ str(len(authors)) + '\n')
 
 number_of_authors = {}
@@ -41,9 +46,34 @@ for commit in commits:
         else:
             number_of_authors[file] = set([author])
 
-for entry in number_of_authors.keys():
-    print (entry + '\t\t' + str(len(number_of_authors[entry])) + '\t' + str(number_of_authors[entry]))
+#for entry in number_of_authors.keys():
+#    print (entry + '\t\t' + str(len(number_of_authors[entry])) + '\t' + str(number_of_authors[entry]))
 
+#aging
+aging = {}
+for commit in commits:
+    for entry in commit['changes']:
+        if(entry['file'] in aging.keys()):
+            current = aging[entry['file']]
+            found = datetime.strptime(commit['date'], "%Y-%m-%d").date()
+            if(found > current):
+                aging[entry['file']] = found
+        else:
+            aging[entry['file']] = datetime.strptime(commit['date'], "%Y-%m-%d").date()
+
+def monthdelta(d1, d2):
+    delta = 0
+    while True:
+        mdays = monthrange(d1.year, d1.month)[1]
+        d1 += timedelta(days=mdays)
+        if d1 <= d2:
+            delta += 1
+        else:
+            break
+    return delta
+
+for entry in aging.keys():
+    print(entry + '\t\t' + str(1+monthdelta(aging[entry],datetime.date(datetime.today()))))
 
 # [entry
 #  [revision]
